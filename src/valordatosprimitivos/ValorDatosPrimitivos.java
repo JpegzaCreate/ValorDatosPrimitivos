66ccd489e6eb19c1f2c88943dfcf5fddc8c674b6
JpegzaCreate <jpegza@gmail.com>
2021-09-23 10:11:34 +0200

    Initial commit

---
+++ src/valordatosprimitivos/ValorDatosPrimitivos.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package valordatosprimitivos;
+
+/**
+ *
+ * @author javip
+ */
+public class ValorDatosPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte numeroByte= 5;
+        System.out.println("nuestro numero byte es: " + numeroByte);
+        System.out.println("El valor mínimo del tipo byte: " + Byte.MIN_VALUE);
+        System.out.println("El valor máximo del tipo byte: " + Byte.MAX_VALUE);
+        
+        
+    }
+    
+}
